eb2645006a5178f031df6bbf9fdade14592ddc31
tapesh <tapeshvashisth789@gmail.com>
2023-10-12 09:38:53 +0530

    check route

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("hello from test");
+    res.send("hello from server");
+})
+
+app.get('/check', (req, res) => {
+    return res.status(200);
 })
 
 app.listen(3000, () => {
-    console.log("test listening on port 3000")
+    console.log("server listening on port 3000")
 })
